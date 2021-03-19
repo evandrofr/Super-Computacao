a60836476c33800b1597b7713602f216c4081cd6
supercomp <evandrofr@al.insper.edu.br>
2021-03-19 18:23:14 +0000

    busca_exaustiva

--- Aula08/tarefa01.cpp
+++ Aula08/tarefa01.cpp
@@ -5,13 +5,55 @@
 
 using namespace std;
 
-int busca_exaustiva(){
+struct item{
+    int peso;
+    int valor;
+    int idx;
+};
 
-    return ;
+
+int busca_exaustiva(int size, vector<item> itens, int carga_total, int i){
+    if(size == i) return 0;
+    int comI;
+    int semI = busca_exaustiva(size, itens, carga_total, i+1);
+    if(carga_total - itens[i].peso >= 0){
+        comI = busca_exaustiva(size, itens, carga_total - itens[i].peso, i+1) + itens[i].valor;
+    } else {
+        comI = semI;
+    }
+
+    return max(comI, semI);
 }
 
 
 int main(){
+
+    vector<item> itens;
+
+    int size;
+    int maxPeso;
+    cout << "Quantos pontos terão a matriz?\n";
+    cin >> size;
+    cout << "Qual o peso máximo da mochila?\n";
+    cin >> maxPeso;
+
+    struct item tmp;
+    for(int i = 0; i < size; i++){
+        cout << "(" << i + 1 << "/" << size << ")\n";
+        cout << "Entre com o peso: \n";
+        cin >> tmp.peso;
+        cout << "Entre com o valor: \n";
+        cin >> tmp.valor;
+        tmp.idx = i;
+        itens.push_back(tmp);
+    }
+
+    cout << "-----------------------------------------------------------------\n";
+
+    int valor_mochila;
+    valor_mochila = busca_exaustiva(size, itens, maxPeso, 0);
+
+    cout << valor_mochila << "\n";
 
     return 0;
 }
