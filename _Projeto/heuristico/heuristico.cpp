fc6b57c901e350978201b4c6beb5d1e23caca618
supercomp <evandrofr@al.insper.edu.br>
2021-03-22 21:35:02 +0000

    Passando nos testes

--- _Projeto/heuristico/heuristico.cpp
+++ _Projeto/heuristico/heuristico.cpp
@@ -11,6 +11,7 @@ struct item{
 
 
 bool compareValor (item &a, item &b) { return a.valor > b.valor; } // > para decrescente e < para crescente
+bool compareIdx   (item &a, item &b) { return a.idx < b.idx; } // > para decrescente e < para crescente
 
 int main(){
     vector<item> itens;
@@ -18,15 +19,15 @@ int main(){
 
     int size;
     int pessoas;
-    cout << "Quantos objetos para dividir?\n";
+    // cout << "Quantos objetos para dividir?\n";
     cin >> size;
-    cout << "Quantas pessoas para dividir?\n";
+    // cout << "Quantas pessoas para dividir?\n";
     cin >> pessoas;
 
     struct item tmp;
     for(int i = 0; i < size; i++){
-        cout << "(" << i + 1 << "/" << size << ")\n";
-        cout << "Entre com o valor: \n";
+        // cout << "(" << i + 1 << "/" << size << ")\n";
+        // cout << "Entre com o valor: \n";
         cin >> tmp.valor;
         tmp.idx = i;
         itens.push_back(tmp);
@@ -35,21 +36,21 @@ int main(){
     sort(itens.begin(), itens.end(), compareValor);
 
 
-    cout << "----------------------------------------------\n";
-    for(int i = 0; i < size; i++){
-        cout << itens[i].idx << " ";
-    }
-    cout << "\n";
+    // cout << "----------------------------------------------\n";
+    // for(int i = 0; i < size; i++){
+    //     cout << itens[i].idx << " ";
+    // }
+    // cout << "\n";
 
-    vector<vector<int>> resposta;
+    vector<vector<item>> resposta;
     for(int i = 0; i < pessoas; i++){
-        vector<int> tmp;
+        vector<item> tmp;
         resposta.push_back(tmp);
     }
 
     for(int i = 0; i < size; i++){
         int index = i%pessoas;
-        resposta[index].push_back(itens[i].valor);
+        resposta[index].push_back(itens[i]);
     }
 
     vector<int> lens;
@@ -58,23 +59,27 @@ int main(){
         lens.push_back(len);
     }
     int minimo = 999999;
-    int idx_minimo = 0;
+    // int idx_minimo = 0;
     for(int i = 0; i < pessoas; i++){
         int total_parcial = 0;
         for(int j = 0; j < lens[i]; j++){
-            total_parcial += resposta[i][j];
+            total_parcial += resposta[i][j].valor;
         }
         if (total_parcial < minimo){
             minimo = total_parcial;
-            idx_minimo = i;
+            // idx_minimo = i;
         }
     }
-    cout << "MMS " << idx_minimo << " " << minimo << "\n";
+
+    for(int i = 0; i < pessoas; i++){
+        sort(resposta[i].begin(), resposta[i].end(), compareIdx);
+    }
+
+    cout << minimo << "\n";
     
     for(int i = 0; i < pessoas; i++){
-        cout << "Vetor " << i << ": ";
         for(int j = 0; j < lens[i]; j++){
-            cout <<resposta[i][j] << " ";
+            cout << resposta[i][j].idx << " ";
         }
         cout << "\n";
     }
